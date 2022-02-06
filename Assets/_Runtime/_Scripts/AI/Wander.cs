a53117a8bfcfad1a3470e508913f25507616cf5f
George Mavroeidis <gdmavroeidis@hotmail.com>
2022-02-06 17:14:24 -0500

    Add Wander functionality

--- Assets/_Runtime/_Scripts/AI/Wander.cs
+++ Assets/_Runtime/_Scripts/AI/Wander.cs
@@ -9,15 +9,38 @@ namespace AI
         protected float wanderTimer = 0;
 
         private Vector3 lastWanderDirection;
+        private Vector3 lastDispalcement;
 
         public override SteeringOutput GetKinematic(AIAgent agent)
         {
             var output = base.GetKinematic(agent);
             wanderTimer += Time.deltaTime;
 
+            if (lastWanderDirection == Vector3.zero)
+            {
+                lastWanderDirection = transform.forward.normalized * agent.maxSpeed;
+            }
+
+            if (lastDispalcement == Vector3.zero)
+            {
+                lastDispalcement = transform.forward;
+            }
+            
             Vector3 desiredVelocity = output.linear;
             // TODO: calculate linear component
+            if (wanderTimer > wanderInterval)
+            {
+                float angle = (Random.value - Random.value) * wanderDegreesDelta;
+                Vector3 direction = Quaternion.AngleAxis(angle, Vector3.up) * lastWanderDirection.normalized;
+                Vector3 circleCenter = transform.position + lastDispalcement;
+                Vector3 destination = circleCenter + direction.normalized;
+                desiredVelocity = destination - transform.position;
+                desiredVelocity = desiredVelocity.normalized * agent.maxSpeed;
 
+                lastDispalcement = desiredVelocity;
+                lastWanderDirection = direction;
+                wanderTimer = 0;
+            }
 
             output.linear = desiredVelocity;
 			

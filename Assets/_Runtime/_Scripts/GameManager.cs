5255bf947e6679ddfd94d39f001400c9c0c0a0a7
George Mavroeidis <gdmavroeidis@hotmail.com>
2022-02-13 14:33:03 -0500

    Add comments and cone target

--- Assets/_Runtime/_Scripts/GameManager.cs
+++ Assets/_Runtime/_Scripts/GameManager.cs
@@ -9,14 +9,21 @@ namespace _Runtime._Scripts
     {
         public static GameManager Instance;
 
+        // List of players in the game
         [SerializeField] private List<AIAgent> _players = new List<AIAgent>();
+        // 0: Red (Pursuer), 1: Green (Wanderer/Target), 2: Yellow (Frozen)
         public List<Material> _cMaterials = new List<Material>();
+        public Transform _targetCone;
 
+        // current pursuer
         public AIAgent _pursuer;
+        // maximum distance between target and pursuer to freeze
         public float _tagDistance = 1f;
         
+        // Map's offset bounds
         public float maxOffset = 40f;
 
+        // Setting up singleton GameManager.cs
         private void Awake()
         {
             if (Instance != null)
@@ -32,12 +39,14 @@ namespace _Runtime._Scripts
         // Start is called before the first frame update
         void Start()
         {
+            // Set all players to wander
             foreach (var player in _players)
             {
                 player.gameObject.AddComponent<Wander>();
                 player.gameObject.AddComponent<LookWhereYouAreGoing>();
                 player.transform.rotation = Quaternion.Euler(0f, Random.Range(-180f, 180f), 0f);
             }
+            // Set up pursuer
             SetPursuer(_players[Random.Range(0, _players.Count)]);
         }
 
@@ -45,14 +54,16 @@ namespace _Runtime._Scripts
         void Update()
         {
             CheckIfTagged();
+            SetConeToTarget();
         }
 
+        // Set selected agent as pursuer
         private void SetPursuer(AIAgent agent)
         {
             _pursuer = agent;
             Destroy(_pursuer.gameObject.GetComponent<Wander>());
             Destroy(_pursuer.gameObject.GetComponent<LookWhereYouAreGoing>());
-            _pursuer.maxSpeed *= 1.5f;
+            _pursuer.maxSpeed *= 1.8f;
             _pursuer.SetMaterial(_cMaterials[0]);
             _pursuer._ePlayerState = PlayerState.EPlayerState.Tagged;
             _pursuer.TrackTarget(FindNearestTargetToFreeze().transform);
@@ -63,14 +74,14 @@ namespace _Runtime._Scripts
         private AIAgent FindNearestTargetToFreeze()
         {
             AIAgent closestPlayer = null;
-            float closestDistanceSqr = Mathf.Infinity;
+            var closestDistanceSqr = Mathf.Infinity;
             foreach (var player in _players)
             {
                 // skip if it compares itself or someone who is already frozen
                 if (player._ePlayerState == PlayerState.EPlayerState.Tagged || player._ePlayerState == PlayerState.EPlayerState.Frozen) continue;
 
                 var distance = player.transform.position - _pursuer.transform.position;
-                float dSqrToTarget = distance.sqrMagnitude;
+                var dSqrToTarget = distance.sqrMagnitude;
                 // check if current comparison is smaller than the current closest player to target
                 if (dSqrToTarget < closestDistanceSqr)
                 {
@@ -81,6 +92,7 @@ namespace _Runtime._Scripts
 
             if (closestPlayer != null)
             {
+                // Destroy components if they exist
                 if (closestPlayer.GetComponent<Wander>() != null)
                 {
                     Destroy(closestPlayer.GetComponent<Wander>());
@@ -102,13 +114,13 @@ namespace _Runtime._Scripts
         private AIAgent FindNearestTargetToUnfreeze(AIAgent frozenGuy)
         {
             AIAgent closestPlayer = null;
-            float closestDistanceSqr = Mathf.Infinity;
+            var closestDistanceSqr = Mathf.Infinity;
             foreach (var player in _players)
             {
                 if (player._ePlayerState == PlayerState.EPlayerState.Unfrozen)
                 {
                     var distance = player.transform.position - frozenGuy.transform.position;
-                    float dSqrToTarget = distance.sqrMagnitude;
+                    var dSqrToTarget = distance.sqrMagnitude;
                     // check if current comparison is smaller than the current closest player to target
                     if (dSqrToTarget < closestDistanceSqr)
                     {
@@ -141,21 +153,30 @@ namespace _Runtime._Scripts
         void CheckIfTagged()
         {
             if (_pursuer == null) return;
-            if (Vector3.Distance(_pursuer.TargetPosition, _pursuer.transform.position) <= _tagDistance)
-            {
-                Destroy(_pursuer.trackedTarget.gameObject.GetComponent<Flee>());
-                Destroy(_pursuer.trackedTarget.gameObject.GetComponent<FaceAway>());
-                var target = _pursuer.trackedTarget.gameObject.GetComponent<AIAgent>();
-                target.behaviorType = AIAgent.EBehaviorType.Kinematic;
-                target.gameObject.AddComponent<Stop>();
-                target.SetMaterial(_cMaterials[2]);
-                target._ePlayerState = PlayerState.EPlayerState.Frozen;
-                target.UnTrackTarget();
+            if (!(Vector3.Distance(_pursuer.TargetPosition, _pursuer.transform.position) <= _tagDistance)) return;
+            
+            Destroy(_pursuer.trackedTarget.gameObject.GetComponent<Flee>());
+            Destroy(_pursuer.trackedTarget.gameObject.GetComponent<FaceAway>());
+            var target = _pursuer.trackedTarget.gameObject.GetComponent<AIAgent>();
+            target.behaviorType = AIAgent.EBehaviorType.Kinematic;
+            target.gameObject.AddComponent<Stop>();
+            target.SetMaterial(_cMaterials[2]);
+            target._ePlayerState = PlayerState.EPlayerState.Frozen;
+            target.UnTrackTarget();
 
-                _pursuer.UnTrackTarget();
-                _pursuer.trackedTarget = FindNearestTargetToFreeze().transform;
+            _pursuer.UnTrackTarget();
+            // Set both pursuer and chased agent to be targets of one another
+            _pursuer.trackedTarget = FindNearestTargetToFreeze().transform;
                 
-                target.TrackTarget(FindNearestTargetToUnfreeze(target).transform);
+            target.TrackTarget(FindNearestTargetToUnfreeze(target).transform);
+        }
+        
+        // Floating game object that indicates the pursuer's current target
+        void SetConeToTarget()
+        {
+            if (_pursuer.trackedTarget != null)
+            {
+                _targetCone.position = _pursuer.trackedTarget.transform.position + new Vector3(0f, 3f, 0f);
             }
         }
     }
